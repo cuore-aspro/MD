ab8bcfb6ff792faf381e4c70e293af0376c0b7c8
cuore-aspro <139486395+cuore-aspro@users.noreply.github.com>
2023-07-31 16:07:46 +0200

    Update timer.py

--- timer.py
+++ timer.py
@@ -1,15 +1,3 @@
-# import time
-
-# def timer (durata):
-#     print(f'il tempo e stato impostato per {durata} secondi.')
-#     time.sleep(durata)
-#     print('il tempo e scaduto')
-# durata = 10
-# timer(durata)
-
-
-
-
 import time 
 
 def timer (durata):
