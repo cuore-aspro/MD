cc19165e2f48f8602d265480d013e433827acd71
cuore-aspro <139486395+cuore-aspro@users.noreply.github.com>
2024-01-19 19:19:27 +0100

    Add files via upload

--- primi passi Pandas.py
+++ primi passi Pandas.py
@@ -2,31 +2,27 @@
 import pandas as pd
 import numpy as np
 
-# creazione di una serie
-serie = pd.Series([np.arange(0,10)])
+
+# Creazione di una serie
+serie = pd.Series([np.arange(0, 10)])
 print(serie)
 
-# esserecizio 1
-'''analisi delle vendite di un negozio :hai un dataframe con le seguenti colonne:'''
-#data,prodoto,quantita,prezzo unitario
-
-
-data = {'Data':['2023-01-02','2023-01-02','2023-01-03,''2023-01-03','2023-01-04'],
-                       'prodotto':['penna','matita','penna','matita','penna'],
-                       'quantita':[10,20,15,5,10],
-                       'prezzo_unitario':[1.2,0.8,1.2,0.8,1.2]}
+# Esercizio 1# Analisi delle vendite di un negozio: hai un dataframe con le seguenti colonne:# data, prodotto, quantita, prezzo unitario
+data = {'Data': ['2023-01-02', '2023-01-02', '2023-01-03', '2023-01-03', '2023-01-04'],
+        'prodotto': ['penna', 'matita', 'gomma', 'astuccio', 'quaderno'], 
+        'quantita': [10, 20, 15, 5, 10], 
+        'prezzo_unitario': [1.2, 0.8, 1.2, 0.8, 1.2]}
 vendite = pd.DataFrame(data)
 
-#fatturato totale per prodotto:
 vendite['fatturato'] = vendite['quantita'] * vendite['prezzo_unitario'] #creo una nuova colonna fatturato
-fatturato_prodotto = vendite.groupby('prodotto')['fatturato'].sum() #raggruppo per prodotto e sommo il fatturato
-print(f'fatturato totale per prodotto:\n, {fatturato_prodotto}\n')
+fatturato_prodotto = vendite.groupby('prodotto')['fatturato'].sum()    #raggruppo per prodotto e sommo il fatturato al gruppo prodotto
+print(f'Fatturato totale per prodotto:\n{fatturato_prodotto}\n')
 
-#prodotto piu' venduto in termini di qunatita:
-prodoto_piu_venduto = vendite.groupby('prodotto')['quantita'].sum().idxmax() #raggruppo per prodotto e sommo la quantita'
-print(f'prodotto piu\' venduto:\n, {prodoto_piu_venduto}\n')
+# Prodotto più venduto in termini di quantità
+prodotto_piu_venduto = vendite.groupby('prodotto')['quantita'].sum().idxmax() #raggruppo per prodotto e sommo la quantita' al gruppo prodotto e trovo l'indice del valore massimo
+print(f'Prodotto più venduto:\n{prodotto_piu_venduto}\n')
 
-# creaiamo uno nuvo dataframe con le vendite mensili:
+# Creiamo un nuovo dataframe con le vendite mensili
 vendite['Data'] = pd.to_datetime(vendite['Data']) #trasformo la colonna data in formato datetime
 fatturato_mensile = vendite.groupby(vendite['Data'].dt.to_period('M'))['fatturato'].sum() #raggruppo per mese e sommo il fatturato con il metodo dt.to_period('M')
-print(f'fatturato mensile:\n, {fatturato_mensile}\n')
+print(f'Fatturato mensile:\n{fatturato_mensile}\n')
