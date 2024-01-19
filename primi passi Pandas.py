cc991fbd89694197a627b47b30e03cfa3f9cdcc8
cuore-aspro <139486395+cuore-aspro@users.noreply.github.com>
2024-01-19 18:41:36 +0100

    Add files via upload

---
+++ primi passi Pandas.py
@@ -0,0 +1,30 @@
+
+import pandas as pd
+import numpy as np
+
+# creazione di una serie
+serie = pd.Series([np.arange(0,10)])
+print(serie)
+
+# esserecizio 1
+'''analisi delle vendite di un negozio :hai un dataframe con le seguenti colonne:'''
+#data,prodoto,quantita,prezzo unitario
+
+
+vendite = {'Data':['2023-01-02','2023-01-02','2023-01-03,''2023-01-03','2023-01-04'],
+                       'prodotto':['penna','matita','penna','matita','penna'],
+                       'quantita':[10,20,15,5,10],
+                       'prezzo unitario':[1.2,0.8,1.2,0.8,1.2]}
+vendite = pd.DataFrame(vendite)
+
+#fatturato totale per prodotto:
+vendite['fatturato'] = vendite['quantita'] * vendite['prezzo unitario'] #creo una nuova colonna fatturato
+fatturato_prodotto = vendite.groupby('prodotto')['fatturato'].sum() #raggruppo per prodotto e sommo il fatturato
+print(f'fatturato totale per prodotto:\n, {fatturato_prodotto}\n')
+
+#prodotto piu' venduto in termini di qunatita:
+prodoto_piu_venduto = vendite.groupby('prodotto')['quantita'].sum().idxmax() #raggruppo per prodotto e sommo la quantita'
+print(f'prodotto piu\' venduto:\n, {prodoto_piu_venduto}\n')
+
+# creaiamo uno nuvo dataframe con le vendite mensili:
+vendite['Data'] = pd.to_datetime(vendite['Data']) #trasformo la colonna data in formato datetime
